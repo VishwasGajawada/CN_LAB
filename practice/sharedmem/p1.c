fd68bbdcee8a83ba2daf7f338b950382f99e0b46
VishwasGajawada <vishwasgajawada2002@gmail.com>
2022-01-13 18:33:37 +0530

    shared memory taking input

--- practice/sharedmem/p1.c
+++ practice/sharedmem/p1.c
@@ -16,11 +16,19 @@ int main(){
     if(str == (char *)-1){perror("shmat"); return 1;}
 
     // write to shared memory
-    char *s = str;
-    *s++ = 'A';
-    for (char c = 'a'; c <= 'z'; c++)
-        *s++ = c;
-    *s = 0;
+    /* this works */
+    // char *s = str;
+    // *s++ = 'A';
+    // for (char c = 'a'; c <= 'z'; c++)
+    //     *s++ = c;
+    // *s = 0;
+
+    /* this also works */
+    scanf("%s", str);
+
+    /*** getline doesnt work because *str is reassigned to some diff address, so shmdt() throws Invalid argument error ***/
+    // size_t len = 0;
+    // getline(&str,&len,stdin);
 
     // detach from shared memory
     if(shmdt(str) < 0){perror("shmdt"); return 1;}
