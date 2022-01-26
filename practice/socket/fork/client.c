efcc9a2e4964b12dcc6b95b423cb317a4f67ac6b
Vishwas Gajawada <gajawa_861911@student.nitw.ac.in>
2022-01-26 14:51:15 +0530

    socket multiple client request using fork

---
+++ practice/socket/fork/client.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h> // strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> // inet_pton
+#include <unistd.h>
+int main() {
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        perror("socket");
+        return 1;
+    }
+
+
+    struct sockaddr_in saddr; // server address
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(8080);
+    if (inet_pton(AF_INET, "127.0.0.1", &saddr.sin_addr) == -1) {
+        perror("inet_pton");
+        return 1;
+    }
+    printf("%s\n", inet_ntoa(saddr.sin_addr));
+
+    // now connect to server
+    int connect_status = connect(sfd, (struct sockaddr*)&saddr, sizeof(saddr));
+    if (connect_status == -1) {
+        perror("connect");
+        return 1;
+    }
+    printf("Connected to server\n");
+
+    // send get request to server
+    char* message = "hello server\n";
+    int send_status = send(sfd, message, strlen(message), 0);
+    if (send_status == -1) {
+        perror("send");
+        return 1;
+    }
+    printf("Message sent\n");
+
+    char server_reply[1024] = { 0 };
+    int recv_status = recv(sfd, server_reply, sizeof(server_reply), 0);
+    if (recv_status == -1) {
+        perror("recv");
+        return 1;
+    }
+    printf("Response received : ");
+    printf("%s\n", server_reply);
+
+    close(sfd);
+
+    return 0;
+}
+
---
+++ practice/socket/fork/server.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h> // strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> // inet_pton
+#include <unistd.h>
+#include <stdlib.h> // exit()
+
+int main() {
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        perror("socket");
+        return 1;
+    }
+
+    struct sockaddr_in saddr; // server address
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(8080);
+    saddr.sin_addr.s_addr = INADDR_ANY;
+
+    int opt = 1;
+    // Forcefully attaching socket to the port, so that bind already in use error doesnt occur
+    if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        return 1;
+    }
+
+    int bind_status = bind(sfd, (struct sockaddr*)&saddr, sizeof(saddr));
+    if (bind_status == -1) {
+        perror("bind");
+        return 1;
+    }
+
+    int listen_status = listen(sfd, 5);
+    if (listen_status == -1) {
+        perror("listen");
+        return 1;
+    }
+
+    struct sockaddr_in caddr;
+    socklen_t caddr_len = sizeof(caddr);
+
+    printf("Waiting for incoming connections\n");
+    int nsfd;
+    while (nsfd = accept(sfd, (struct sockaddr*)&caddr, &caddr_len)) {
+        if (nsfd == -1) {
+            perror("accept");
+            continue;
+        }
+        int c = fork();
+        if (c > 0) {
+
+        }
+        else {
+            printf("handler assigned\n");
+            char msg[1024] = { 0 };
+            int recv_status = recv(nsfd, msg, sizeof(msg), 0);
+            if (recv_status == -1) {
+                perror("recv");
+                exit(0);
+            }
+            printf("Response received : ");
+            printf("%s", msg);
+            strcpy(msg, "hello client\n");
+            write(nsfd, msg, strlen(msg));
+            close(nsfd);
+            exit(0);
+        }
+    }
+    close(nsfd);
+    close(sfd);
+    return 0;
+}
